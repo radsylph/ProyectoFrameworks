7aa8df09788ec123b2cc3467468847e355106495
agetro2003 <jesusdaniolob@gmail.com>
2023-06-25 20:03:25 -0400

    Add Dialog component

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -1,35 +1,85 @@
 import { useEffect, useState } from "react"
-
+import './Dialog.css'
 export default function Dialog(){
 
     const [tables, setTables] = useState ( [
         {
-        name: "empleado"
+        name: "empleado",
+        columns: [
+            {
+                name: "id",
+                type: "string",
+                FK: false
+            }, 
+        "descric"] 
+
     },
     {
-        name: "cliente"
+        name: "cliente",
+        columns: ["id", "descric", "id_cargo"]
     },
     {
-        name: "factura"
+        name: "factura",
+        columns: ["id", "descric", "fecha"]
     },
     {
-        name: "cargo"
+        name: "cargo",
+        columns: ["id", "descric"]
     }
 ])
-const [selectedTable, setSelectedTable] = useState("")
+const [selectedTable, setSelectedTable] = useState({})
 const handler = (e) => {
-    setSelectedTable(e.target.value)
+    let table = tables.filter(t =>{
+        return t.name == e.target.value
+    })
+    console.log(table[0])
+    setSelectedTable(table[0])
 }
 
 
 return(
     <>
-        <select name="table" id="table" onChange={handler}>
+        <select name="table" id="table" defaultValue={"DEFAULT"} onChange={handler}>
+            <option value="DEFAULT" disabled  hidden >Seleccione una tabla</option>
             {
                 tables.map((t,idx) => <option key={idx}>{t.name}</option>)
             }
         </select>
-            <h1>{selectedTable}</h1>
+            <h1>{selectedTable.name}</h1>
+            <div>
+            {selectedTable.columns?selectedTable.columns.map((c,idx) => 
+                <div key={idx}>
+                <label>{c}</label> 
+                <input type="text" />              
+                </div>
+                ):<></>}
+            <table>
+                
+                <thead>
+                <tr>
+                {selectedTable.columns?selectedTable.columns.map((c,idx) => 
+                <th key={idx}>{c}</th> 
+                ):<th></th>}
+                </tr>       
+                </thead>
+                <tbody>
+                 <tr>
+                 {selectedTable.columns?selectedTable.columns.map((c,idx) => 
+                <td key={idx}></td> 
+                ):<td></td>}
+                </tr>
+                 
+                </tbody>
+                
+            </table>
+
+            <button>insertar</button>
+            <button>modificar</button>
+            <button>eliminar</button>
+            <button>actualizar</button>
+            <button>salir</button>
+
+            </div>
     </>
 )
 }
