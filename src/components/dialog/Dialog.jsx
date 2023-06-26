1930954bad32158fcfbcf455d4e8068f4a6d38a9
radsylph <andresalvarezsilva6@gmail.com>
2023-06-26 01:13:05 -0400

    feat: added dynamic load for the tables and tables colunm from the database

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -1,85 +1,114 @@
-import { useEffect, useState } from "react"
-import './Dialog.css'
-export default function Dialog(){
+import { useEffect, useState } from "react";
+import "./Dialog.css";
+import axios from "axios";
+export default function Dialog() {
+  const [tables, setTables] = useState([]);
 
-    const [tables, setTables] = useState ( [
-        {
-        name: "empleado",
-        columns: [
-            {
-                name: "id",
-                type: "string",
-                FK: false
-            }, 
-        "descric"] 
+  const table_name = () => {
+    axios
+      .get("http://localhost:4000/tables")
+      .then((response) => {
+        const tableNames = response.data.data.map((table) => table.tablename);
+        tableNames.forEach((tableName) => {
+          table_columns(tableName);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    },
-    {
-        name: "cliente",
-        columns: ["id", "descric", "id_cargo"]
-    },
-    {
-        name: "factura",
-        columns: ["id", "descric", "fecha"]
-    },
-    {
-        name: "cargo",
-        columns: ["id", "descric"]
-    }
-])
-const [selectedTable, setSelectedTable] = useState({})
-const handler = (e) => {
-    let table = tables.filter(t =>{
-        return t.name == e.target.value
-    })
-    console.log(table[0])
-    setSelectedTable(table[0])
-}
+  const table_columns = (tableName) => {
+    axios
+      .get(`http://localhost:4000/columns/${tableName}`)
+      .then((response) => {
+        const tableColumns = response.data.data[0].columns;
+        console.log(tableColumns);
+        addTable(tableName, tableColumns); // Llamamos a addTable aquí después de obtener los datos de la API
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  const addTable = (name, columns) => {
+    setTables((prevTables) => [
+      ...prevTables,
+      {
+        name: name,
+        columns: columns,
+      },
+    ]);
+  };
 
-return(
+  const [selectedTable, setSelectedTable] = useState({});
+
+  const handler = (e) => {
+    let table = tables.filter((t) => {
+      return t.name == e.target.value;
+    });
+    console.log(table[0]);
+    setSelectedTable(table[0]);
+  };
+
+  return (
     <>
-        <select name="table" id="table" defaultValue={"DEFAULT"} onChange={handler}>
-            <option value="DEFAULT" disabled  hidden >Seleccione una tabla</option>
-            {
-                tables.map((t,idx) => <option key={idx}>{t.name}</option>)
-            }
-        </select>
-            <h1>{selectedTable.name}</h1>
-            <div>
-            {selectedTable.columns?selectedTable.columns.map((c,idx) => 
-                <div key={idx}>
-                <label>{c}</label> 
-                <input type="text" />              
-                </div>
-                ):<></>}
-            <table>
-                
-                <thead>
-                <tr>
-                {selectedTable.columns?selectedTable.columns.map((c,idx) => 
-                <th key={idx}>{c}</th> 
-                ):<th></th>}
-                </tr>       
-                </thead>
-                <tbody>
-                 <tr>
-                 {selectedTable.columns?selectedTable.columns.map((c,idx) => 
-                <td key={idx}></td> 
-                ):<td></td>}
-                </tr>
-                 
-                </tbody>
-                
-            </table>
-
-            <button>insertar</button>
-            <button>modificar</button>
-            <button>eliminar</button>
-            <button>actualizar</button>
-            <button>salir</button>
-
+      <select
+        name="table"
+        id="table"
+        defaultValue={"DEFAULT"}
+        onChange={handler}
+      >
+        <option value="DEFAULT" disabled hidden>
+          Seleccione una tabla
+        </option>
+        {tables.map((t, idx) => (
+          <option key={idx}>{t.name}</option>
+        ))}
+      </select>
+      <h1>{selectedTable.name}</h1>
+      <div>
+        {selectedTable.columns ? (
+          selectedTable.columns.map((c, idx) => (
+            <div key={idx}>
+              <label>{c}</label>
+              <input type="text" />
             </div>
+          ))
+        ) : (
+          <></>
+        )}
+        <table>
+          <thead>
+            <tr>
+              {selectedTable.columns ? (
+                selectedTable.columns.map((c, idx) => <th key={idx}>{c}</th>)
+              ) : (
+                <th></th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {selectedTable.columns ? (
+                selectedTable.columns.map((c, idx) => <td key={idx}></td>)
+              ) : (
+                <td></td>
+              )}
+            </tr>
+          </tbody>
+        </table>
+
+        <button>insertar</button>
+        <button>modificar</button>
+        <button>eliminar</button>
+        <button>actualizar</button>
+        <button>salir</button>
+      </div>
+
+      <aside>
+        <button onClick={table_name}>test</button>
+      </aside>
     </>
-)
+  );
 }
