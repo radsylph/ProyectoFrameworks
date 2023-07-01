914571dc537a511e5fad44746e9e7ef236765c2b
radsylph <andresalvarezsilva6@gmail.com>
2023-07-01 01:35:32 -0400

    fixes void element declaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import Dialog from './components/dialog/Dialog'
-import './App.css'
+import { useState } from "react";
+import Dialog from "./components/dialog/Dialog";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-   <Dialog></Dialog>
+      <Dialog />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
